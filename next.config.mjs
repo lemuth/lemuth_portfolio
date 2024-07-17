85051a3b8c1eac79bea2ac364c44454914ddefaf
lemuth <gilles.saley@gmail.com>
2024-07-17 10:27:50 +0200

    changement workflow/publish.yml

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@ const nextConfig = {
   basePath: "/lemuth_portfolio",
   output: "export",
   // reactStrictMode: true,
-  // images: {
-  //   loader: "akamai",
-  //   path: "",
-  // },
-  // assetPrefix: "",
-  // images: {
-  //   unoptimized: true
-  // }
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "/public",
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
