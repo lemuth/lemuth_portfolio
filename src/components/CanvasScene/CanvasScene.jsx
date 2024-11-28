5131906b3ac0bd810a3755c478753cdf03f8488c
lemuth <gilles.saley@gmail.com>
2024-11-28 09:58:38 +0100

    fix sound

--- src/components/CanvasScene/CanvasScene.jsx
+++ src/components/CanvasScene/CanvasScene.jsx
@@ -239,8 +239,8 @@ export default function CanvasScene(props) {
                     <Image src={rideauLat} alt="rideauLateralRight" className={`w-1/2 h-full ${openingLat && "transition-all duration-1000 ease-in translate-x-[100%]"}`}  />
                 </div>
                 {/* Sound */}
-                <audio src="./lemuth_portfolio/sound/south_park_bus_stop.mp3" ref={spAudioTheme} />
-                <audio src="./lemuth_portfolio/sound/open_stage_curtain.mp3" ref={stageCurtainAudio} />
+                <audio src="/lemuth_portfolio/sound/south_park_bus_stop.mp3" ref={spAudioTheme} />
+                <audio src="/lemuth_portfolio/sound/open_stage_curtain.mp3" ref={stageCurtainAudio} />
             </div>
             {/* Helper Bottom pannel */}
             <div>
